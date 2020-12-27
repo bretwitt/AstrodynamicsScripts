9ecbac32fb8cefa0f26aad69d738c18755cd1d72
B.W. <legonxtboy@gmail.com>
2020-12-28 04:07:00 +0900

    Gibbs method

--- Chapter2/ISSOrbit.m
+++ Chapter2/ISSOrbit.m
@@ -6,8 +6,8 @@ addpath OrbitDetermination
 % Example vectors of the ISS 12/26/20 converted to DUe or DUe/TU
 % Accessed from https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html
 
-r = [-0.815 0.3133 0.6086];
-v = [0.0812 -0.8109260 0.5247];
+r = [0.292768687676 -0.838731022264 0.587159656632];
+v = [0.795986637698 -0.103850330044 -0.543736570272];
 
 elements = determineorbit(r,v);
 %plotorbit(elements(1),elements(2),elements(3));
